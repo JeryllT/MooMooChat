6b0d178ca4b1f41cbe07398cf28684e1a173cd94
JeryllT <100482755+JeryllT@users.noreply.github.com>
2023-03-12 14:12:20 +0800

    Gridlines added to segment chat interface

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,15 +1,26 @@
 import ChatStyles from '../styles/Chat.module.css';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 
 const Chat = () => {
 
     return (
             <div className={ChatStyles['grid-container']}>
                 <Card className={ChatStyles['chat-container']}>
-                    <CardContent>
-                        test
-                    </CardContent>
+                    <div className={ChatStyles['header-left']}>
+                        Moomoo Chat
+                    </div>
+                    <div className={ChatStyles['searchbar']}>
+                        Search Bar
+                    </div>
+                    <div className={ChatStyles['contacts']}>
+                        contacts
+                    </div>
+                    <div className={ChatStyles['header-right']}>
+                        User details
+                    </div>
+                    <div className={ChatStyles['chat']}>
+                        Current Message
+                    </div>
                 </Card>
             </div>
     )
